f51a298ab7b326ee6abaaadd9fca6384c4979f07
nikita <nicita.zinkevitch@yandex.ru>
2024-03-13 21:38:27 +0300

    Init routes

--- internal/api/handler/handler.go
+++ internal/api/handler/handler.go
@@ -2,8 +2,10 @@ package handler
 
 import (
 	"fmt"
+	"github.com/Warh40k/vk-intern-filmotecka/internal/api/service"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"log/slog"
-	"net/http"
 )
 
 type Handler struct {
@@ -25,8 +27,30 @@ func (e UploadError) Error() string {
 	return fmt.Sprintf("%s: %s", e.Filename, e.Message)
 }
 
-func (h *Handler) InitRoutes() *http.ServeMux {
-	router := http.ServeMux{}
+func (h *Handler) InitRoutes() *chi.Mux {
+	router := chi.NewRouter()
+	router.Use(middleware.RequestID)
+	router.Use(middleware.Recoverer)
+
+	router.Route("/films", func(r chi.Router) {
+		r.Get("/", h.ListFilms)
+		r.Post("/", h.CreateFilm)
+		r.Get("/search", h.SearchFilm)
+		r.Route("/{film_id}", func(r chi.Router) {
+			r.Put("/", h.UpdateFilm)
+			r.Patch("/", h.PatchFilm)
+			r.Delete("/", h.DeleteFilm)
+		})
+	})
+	router.Route("/actors", func(r chi.Router) {
+		r.Get("/", h.ListActors)
+		r.Post("/", h.CreateActor)
+		r.Route("/{actor_id}", func(r chi.Router) {
+			r.Patch("/", h.PatchActor)
+			r.Put("/", h.UpdateActor)
+			r.Delete("/", h.DeleteActor)
+		})
+	})
 
 	return router
 }
